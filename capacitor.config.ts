c36ad41fd4832127b59042b1c12f699b61e5da46
SebaNeira26 <you@example.com>
2024-09-07 17:18:23 -0400

    scss listos y modifique un poco los html , por los botones y unos arreglos

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RegisterDuocApp',
+  webDir: 'www'
+};
+
+export default config;
