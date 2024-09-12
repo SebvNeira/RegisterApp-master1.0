5e4459f2df2682587bb2472af3da0368da0594b2
SebaNeira26 <you@example.com>
2024-09-12 17:53:35 -0300

    Sigan viendo

--- src/app/forgot-password/forgot-password.page.ts
+++ src/app/forgot-password/forgot-password.page.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.page.html',
   styleUrls: ['./forgot-password.page.scss'],
 })
-export class ForgotPasswordPage implements OnInit {
+export class ForgotPasswordPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  recuperarContrasena() {
+    alert('Recuperación en proceso');
+    this.router.navigate(['/login']);
   }
-
 }
+
+  
