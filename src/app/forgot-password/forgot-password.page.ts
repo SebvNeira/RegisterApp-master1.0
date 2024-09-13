4e57d6b32ec8fb6a51577769e089fa1ce7b6c0cc
BMolinae <ben.molina@duocuc.cl>
2024-09-13 00:03:23 -0300

    Se agregaron validaciones y alertas

--- src/app/forgot-password/forgot-password.page.ts
+++ src/app/forgot-password/forgot-password.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,13 +8,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./forgot-password.page.scss'],
 })
 export class ForgotPasswordPage {
+  email:string='';
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private alertController: AlertController) { }
 
-  recuperarContrasena() {
-    alert('Recuperación en proceso');
-    this.router.navigate(['/login']);
+  async recuperarContrasena() {
+    if (!this.email) {
+      await this.mostrarMensaje('Por favor ingresa tu correo electrónico.');
+    
+    } else if (!this.validarEmail(this.email)) {
+      await this.mostrarMensaje('Por favor ingresa un correo electrónico válido.');
+    
+    } else {
+      await this.mostrarMensaje('Se ha enviado un enlace de recuperación a tu correo.');
+    }
+
+    this.email = '';
   }
+
+  validarEmail(email: string): boolean{
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  async mostrarMensaje(mensaje: string){
+    const alert = await this.alertController.create({
+      header: 'Recuperacion de Contraseña',
+      message: mensaje,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
 }
 
   
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage{
 
-  constructor() { }
+  username: string='';
+  password: string='';
 
-  ngOnInit() {
+  constructor(private router: Router, private alertCotroller: AlertController) { }
+
+  async iniciarSesion() {
+    const adminCredentials = { username: 'admin', password: 'admin123' };
+    const userCredentials = { username: 'usuario', password: 'user123' };
+
+    if(this.username === adminCredentials.username && this.password === adminCredentials.password){
+      await this.mostrarMensaje('Bienvenido, Admin');
+
+    } else if (this.username === userCredentials.username && this.password === userCredentials.password){
+      await this.mostrarMensaje('Bienvenido, Usuario');
+      
+    } else {
+      await this.mostrarMensaje('Credenciales incorrectas, intente nuevamente.');
+    }
+
+    this.username = '';
+    this.password = '';
+  }
+
+  async mostrarMensaje(mensaje: string){
+    const alert = await this.alertCotroller.create({
+      header: 'Inicio de Sesión',
+      message: mensaje,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
   }
 
 }
